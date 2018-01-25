292d08bb1ca9ae16f67f56a3c0ea0b9f3976f27e
Elton Fonseca <eltonfonseca1994@gmail.com>
2018-01-25 23:51:12 +0000

    Linear Search Recursive Algorithm

---
+++ ruby/linearSearchRecursive.rb
@@ -0,0 +1,26 @@
+#   Linear Search Algorithm in Ruby
+#
+#   Author: Elton Fonseca
+#
+
+def main()
+    @array = [1,2,3,4,5,6,7,8,9,10]
+
+    if(search(@array, @array.length - 1, 7))
+        puts("Find!")
+    else
+        puts("Not Found!")
+    end 
+end
+
+def search(array, element, value)
+    if(element < 0)
+        return false
+    elsif(array[element] == value)
+        return true
+    else
+        return search(array, element - 1, value)
+    end
+end
+
+main()
